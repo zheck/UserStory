e70cb92fdf0acc123d74b823bc7da3b9599d32d2
Fong <fong.zhou75@gmail.com>
2025-04-16 12:15:24 +0200

    [0] project init

---
+++ UserStory/UserStory/App/UserStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserStoryApp.swift
+//  UserStory
+//
+//  Created by Fong Zhou on 16/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct UserStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
